b09af03fd743010315bedad5bcbcb94c1d927f68
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-07-28 12:55:52 +0200

    create spotlight component

---
+++ components/Spotlight.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
+export default function Spotlight() {
+  const {
+    data: data,
+    error,
+    isLoading,
+  } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+
+  if (error) return <p>Fehler beim Laden der Daten.</p>;
+  if (isLoading) return <p>Lade...</p>;
+
+  function getRandomArtPiece() {
+    const randomNumber = Math.random() * data.length;
+    const randomArrayIndex = Math.floor(randomNumber);
+    return data[randomArrayIndex];
+  }
+  const randomArt = getRandomArtPiece();
+  return (
+    <>
+      <Image
+        src={randomArt.imageSource}
+        width={400}
+        height={400}
+        alt={randomArt.name}
+      />
+      <p>{randomArt.artist}</p>
+    </>
+  );
+}
